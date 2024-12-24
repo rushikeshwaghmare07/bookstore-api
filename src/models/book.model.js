c2b9a68c936fb33f54aa29a95c5a3cdd0bcf4ed5
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-12-24 08:08:01 +0530

    Add Book model schema

---
+++ src/models/book.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Book title is required"],
+      trim: true,
+      maxLength: [100, "Book title can not be more than 100 characters"],
+    },
+    author: {
+      type: String,
+      required: [true, "Author name is required"],
+      trim: true,
+    },
+    year: {
+      type: Number,
+      required: [true, "Publication year is required"],
+      min: [1000, "Year must be at least 1000"],
+      max: [new Date().getFullYear(), "Year can not be in the future"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
